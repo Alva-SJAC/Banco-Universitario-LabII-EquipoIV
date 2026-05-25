fe95b09a7ec88c2b0b97253865bd609a5f16bb00
Luis Daza <ldaza2394@gmail.com>
2026-05-25 17:53:20 -0400

    fix: adjust responsive image height and update handshake icon in About Us section

--- src/components/public/AboutSection.vue
+++ src/components/public/AboutSection.vue
@@ -9,7 +9,7 @@
       </div>
 
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-        <div class="relative rounded-[2rem] h-[450px]">
+        <div class="relative rounded-[2rem] h-[250px] sm:h-[350px] lg:h-[450px]">
 
           <img src="../../assets/img/AboutUs.png" alt="Equipo de trabajo del Banco Universitario" class="w-full h-full object-cover rounded-[2rem] shadow-lg" />
           
@@ -48,11 +48,8 @@
             <!-- Item 2 -->
             <div class="flex items-start space-x-4">
               <div class="flex-shrink-0 w-10 h-10 bg-[#eaf7f6] rounded-xl flex items-center justify-center text-bu-teal mt-0.5">
-                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="m11 17 2 2a1 1 0 1 0 3-3"/>
-                  <path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4"/>
-                  <path d="M21 3 15 9"/>
-                  <path d="m8 16-2-2a1 1 0 1 0-3 3l3.88 3.88a3 3 0 0 0 4.24 0l.88-.88a1 1 0 1 1 3 3l-2.81 2.81a5.79 5.79 0 0 1-7.06.87l-.47-.28a2 2 0 0 0-1.42-.25L3 21"/>
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M21.71 8.71c1.25-1.25.68-2.71 0-3.42l-3-3c-1.26-1.25-2.71-.68-3.42 0L13.59 4H11C9.1 4 8 5 7.44 6.15L3 10.59v4l-.71.7c-1.25 1.26-.68 2.71 0 3.42l3 3c.54.54 1.12.74 1.67.74c.71 0 1.36-.35 1.75-.74l2.7-2.71H15c1.7 0 2.56-1.06 2.87-2.1c1.13-.3 1.75-1.16 2-2C21.42 14.5 22 13.03 22 12V9h-.59zM20 12c0 .45-.19 1-1 1h-1v1c0 .45-.19 1-1 1h-1v1c0 .45-.19 1-1 1h-4.41l-3.28 3.28c-.31.29-.49.12-.6.01l-2.99-2.98c-.29-.31-.12-.49-.01-.6L5 15.41v-4l2-2V11c0 1.21.8 3 3 3s3-1.79 3-3h7zm.29-4.71L18.59 9H11v2c0 .45-.19 1-1 1s-1-.55-1-1V8c0-.46.17-2 2-2h3.41l2.28-2.28c.31-.29.49-.12.6-.01l2.99 2.98c.29.31.12.49.01.6"/>
                 </svg>
               </div>
               <div>
