b13e12caa9402b8fa5c41c863ef386bfcf8ced09
bleazer10 <rualesi.719@gmail.com>
2026-07-18 09:48:43 -0400

    feat: add contact API service

---
+++ src/services/contactService.js
@@ -0,0 +1,38 @@
+import api from './api'
+
+export const contactService = {
+  async getContacts(alias = '') {
+    const query = alias ? `?alias=${encodeURIComponent(alias)}` : ''
+    const response = await api.get(`/v1/client/contact${query}`)
+    return response.data
+  },
+
+  async getContactById(contactId) {
+    const response = await api.get(`/v1/client/contact/${contactId}`)
+    return response.data
+  },
+
+  async createContact(contactData) {
+    const response = await api.post('/v1/client/contact', {
+      alias: contactData.alias,
+      account_number: contactData.account_number,
+      description: contactData.description || null
+    })
+
+    return response.data
+  },
+
+  async updateContact(contactId, contactData) {
+    const response = await api.patch(`/v1/client/contact/${contactId}`, {
+      alias: contactData.alias,
+      description: contactData.description || null
+    })
+
+    return response.data
+  },
+
+  async deleteContact(contactId) {
+    const response = await api.delete(`/v1/client/contact/${contactId}`)
+    return response.data
+  }
+}
